0be61694a2b6a9ab6199ad4db3b30979bc904508
Dilshod Musakhanov <musakhanov@yahoo.com>
2023-08-08 07:23:53 +0400

    RESTFul. Describing its architecture

--- src/main/java/ru/job4j/auth/controller/PersonController.java
+++ src/main/java/ru/job4j/auth/controller/PersonController.java
@@ -36,7 +36,7 @@ public class PersonController {
     public ResponseEntity<Person> create(@RequestBody Person person) {
         var result = personService.save(person);
         return result.map(p -> new ResponseEntity<>(p, HttpStatus.CREATED))
-                .orElseGet(() -> new ResponseEntity<>(null, HttpStatus.CONFLICT));
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.CONFLICT));
     }
 
     @PutMapping("/")
