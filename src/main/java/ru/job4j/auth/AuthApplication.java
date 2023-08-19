2076fa4c97e168e4af2feae6fb581a10fae327aa
Dilshod Musakhanov <musakhanov@yahoo.com>
2023-08-19 21:28:49 +0400

    Spring Liquibase

--- src/main/java/ru/job4j/auth/AuthApplication.java
+++ src/main/java/ru/job4j/auth/AuthApplication.java
@@ -1,11 +1,22 @@
 package ru.job4j.auth;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class AuthApplication {
 
+	@Bean
+	public SpringLiquibase liquibase(DataSource ds) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+		liquibase.setDataSource(ds);
+		return liquibase;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
--- src/main/resources/db/002_dml_insert_person_table.sql
+++ src/main/resources/db/002_dml_insert_person_table.sql
@@ -1,9 +1,3 @@
-create table person (
-    id serial primary key not null,
-    login varchar(2000) not null unique ,
-    password varchar(2000) not null
-);
-
 insert into person (login, password) values ('parsentev', '123');
 insert into person (login, password) values ('ban', '123');
 insert into person (login, password) values ('ivan', '123');
